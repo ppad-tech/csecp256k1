372b6a51cabaeae2dfa0f91c365afad5b0235b80
Jared Tobin <jared@jtobin.io>
2024-02-09 15:25:33 +0400

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "haskell-secp256k1";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.secp256k1
+              pkgs.haskell.compiler.ghc964
+              pkgs.haskell.packages.ghc964.cabal-install
+            ];
+
+            shellHook = ''
+              echo "entering shell.."
+              PS1="\e[1;34m[nix-develop] \w$ \e[0m"
+              echo "$(${pkgs.haskell.compiler.ghc964}/bin/ghc --version)"
+            '';
+          };
+        }
+      );
+}
