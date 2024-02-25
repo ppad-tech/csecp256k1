412340b95bc6334c118486c33242d5e5ccf63137
Jared Tobin <jared@jtobin.io>
2024-02-25 08:59:27 +0400

    Update project flake.

--- flake.nix
+++ flake.nix
@@ -9,20 +9,48 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs  = nixpkgs.legacyPackages.${system};
+        lib = "ppad-csecp256k1";
+        sys = "secp256k1-sys";
+
+        config = {
+          packageOverrides = super: let self = super.pkgs; in rec {
+            haskell = super.haskell // {
+              packageOverrides = self: super: {
+                ${sys} = super.callCabal2nix sys ./${sys} {};
+                ${lib} = super.callCabal2nix lib ./. {};
+              };
+            };
+          };
+        };
+
+        pkgs  = import nixpkgs { inherit system; inherit config; };
         hpkgs = pkgs.haskell.packages.ghc964;
+
+        cc    = pkgs.stdenv.cc;
         ghc   = hpkgs.ghc;
         cabal = hpkgs.cabal-install;
       in
         {
-          devShells.default = pkgs.mkShell {
-            buildInputs = [
-              cabal
+          packages.${lib} = hpkgs.${lib};
+
+          defaultPackage = self.packages.${system}.${lib};
+
+          devShells.default = hpkgs.shellFor {
+            packages = p: [
+              p.${lib} p.${sys}
             ];
 
+            buildInputs = [
+              cabal
+              cc
+            ];
+
+            inputsFrom = builtins.attrValues self.packages.${system};
+
             shellHook = ''
-              PS1="[nix] \w$ "
-              echo "entering shell, using"
+              PS1="[${lib}] \w$ "
+              echo "entering ${system} shell, using"
+              echo "cc:    $(${cc}/bin/cc --version)"
               echo "ghc:   $(${ghc}/bin/ghc --version)"
               echo "cabal: $(${cabal}/bin/cabal --version)"
             '';
@@ -30,3 +58,4 @@
         }
       );
 }
+
